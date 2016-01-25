1f28a83e3d3a27caf8ae739961de1a817406f57e
Samir Mehta <sammehta88@gmail.com>
2016-01-25 18:01:21 -0500

    FIX: migration for values

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122185822) do
+ActiveRecord::Schema.define(version: 20160125205014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "profiles", force: :cascade do |t|
-    t.string  "quote"
-    t.integer "user_id"
+    t.string   "quote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -40,15 +42,20 @@ ActiveRecord::Schema.define(version: 20160122185822) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "values", force: :cascade do |t|
-    t.string  "value"
-    t.integer "user_id"
-    t.integer "profile_id"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "values", ["profile_id"], name: "index_values_on_profile_id", using: :btree
-  add_index "values", ["user_id"], name: "index_values_on_user_id", using: :btree
+  create_table "valuetaggings", force: :cascade do |t|
+    t.integer  "profile_id"
+    t.integer  "value_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "valuetaggings", ["profile_id"], name: "index_valuetaggings_on_profile_id", using: :btree
+  add_index "valuetaggings", ["value_id"], name: "index_valuetaggings_on_value_id", using: :btree
 
   add_foreign_key "profiles", "users"
-  add_foreign_key "values", "profiles"
-  add_foreign_key "values", "users"
 end
