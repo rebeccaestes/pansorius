8ad3d5fc52ce1ee166c31e9f560bb5baa3f4a3b1
Alexandra <akscreven@gmail.com>
2016-01-26 17:30:57 -0500

    added values route

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,6 +14,7 @@ class ProfilesController < ApplicationController
 
   def show
     @experiences = @profile.experiences
+    @values = @profile.values
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   resources :experiences
+  resources :values
   root to: 'profiles#index'
   devise_for :users
-
   resources :profiles
 end
