42297d48f62403605a240b10070a961046e59d43
Samir Mehta <sammehta88@gmail.com>
2016-01-26 16:19:28 -0500

    add create action to profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,20 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
 
+  def create
+    @profile = Profile.new(profile_params)
+
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
+        format.json { render :show, status: :created, location: @profile }
+      else
+        format.html { render :new }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
     def set_profile
