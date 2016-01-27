397fd3e4f27c76021e1abc2c743905334167abe1
Alexandra <akscreven@gmail.com>
2016-01-27 13:48:54 -0500

    changed value routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+
+
   root to: 'pages#index'
   devise_for :users
   resources :profiles do
     resources :experiences
-    resources :values, shallow: true do
-      resources :valuetaggings
+    resources :valuetaggings, shallow: true do
+      resources :values
     end
   end
 
-  get '/mentors', to: 'profiles#mentors'
-  get '/proteges', to: 'profiles#proteges'
 
   get '/about', to: 'pages#about'
   get '/home', to: 'pages#home'
+
+
 end
