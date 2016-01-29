3177f35a68f09be05e3417291eba8d42aaace884
Samir Mehta <sammehta88@gmail.com>
2016-01-29 00:22:41 -0500

    serve static assets;

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Pansorius
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.serve_static_assets = true
   end
 end
