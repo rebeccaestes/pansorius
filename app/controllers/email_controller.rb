34e3c453a499f9b59e97661abdb78d0dd2a7a6d1
Samir Mehta <sammehta88@gmail.com>
2016-02-11 23:11:53 -0500

    modify email message

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,18 +4,20 @@ class EmailController < ApplicationController
 
   def new
     @template =
-"To: Potential #{@recipient.role.capitalize}, #{@recipient.name}
-From: Potential #{current_user.profile.role.capitalize}, #{current_user.profile.name}
-Subject: Introducing Yourself
-
-Welcome to Pansori.us messaging system!
+"Welcome to Pansori.us messaging system!
 
 A great way to begin a mentorship relationship is to reach out and introduce yourself. Tell them a little interesting thing about yourself or what you two seem to share in common.  Keep it short, sweet, and sharp.  Most successful mentorships are organically started with common interests and segways into being a mutually beneficial relationship.  Everyone has something to offer and everyone can benefit to learn.  Think about who you are reaching out to and how can your values or skills can be of service to their specific needs.  You can either try to anticipate what their needs might be or you could ask them insightful questions that displays your thought process.
 
 What are you waiting for?  Let’s get started!
 
 Your Advisors,
-Pansori.us"
+Pansori.us
+
+Instructions:
+              Replace everything in this text field with the email message you would like to send
+              The message written in this box will be sent to your potential mentor/protege.
+              Press the send message button when finished to send the message.
+"
   end
 
   def create
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: ENV['domain'] || ENV['MAILGUN_DOMAIN']
+  default from: "pansori@#{ENV['domain'] || ENV['MAILGUN_DOMAIN']}"
 
   def connect_email(to,from, message)
     @to = to
     @from = from
     @message = message
-    mail to: @to.email, subject: 'Welcome to My Awesome Site'
+    mail to: @to.email, from: @from.profile.name, subject: "Potential #{@from.profile.role.capitalize}"
   end
 end
